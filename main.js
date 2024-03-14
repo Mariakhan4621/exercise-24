fcf14c52d43e6f7de0350aa85e20cd9b0811e235
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-14 20:10:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//let name_1 : string = "maria"
+//let name_2 : string = "maria iqrar"
+//let name_3 : string = "miss maria iqrar"
+// if (name_1 == name_2){
+//    console.log("names are equal")
+// }else {
+//    console.log("names are not equal")
+// }
+// if (name_1 != name_2){
+//    console.log("names are equal")
+// }
+// if (name_1 != name_3){
+//   console.log("names are equal")
+// }
+//let age_1 : number = 18
+//let age_2 : number = 12
+// if (age_1 == 18) {
+//   console.log("eligibal for vote")
+// }
+// if (age_1 != 22) {
+//   console.log("eligibal for vote in older categery")
+// }
+// if (age_1 <= age_2){ //less
+//   console.log("younger")
+// }
+// if (age_2 >= age_1){ //greater
+//    console.log("older")
+// }
+// if (age_1 == 18 && age_2 == 22) {
+//    console.log(" person is eligibal for vote")
+// }
+// if (age_1 == 18 || age_2 != 22) {
+//   console.log(" person is eligibal not for vote")
+// }
+var cityes = ["karachi", "hydrabad", "faisalabad", "gujrat"];
+// if (cities.includes("karachi")){
+//   console.log("karachi is in cities list")
+// }
+if (!cityes.includes("sukhar")) {
+    console.log("sukhar is not includes in an array");
+}
